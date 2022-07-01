47ac5c07ee5881399fded4f64b45effc767aa0eb
ranawp <ranasaha4224#gmail.com>
2022-07-01 12:53:56 +0600

    calender added

--- src/components/pages/Calender.js
+++ src/components/pages/Calender.js
@@ -1,11 +1,18 @@
-import React from 'react';
-
-const Calender = () => {
+import React, { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+const CalendarCompo = () => {
+    const [value, onChange] = useState(new Date());
     return (
-        <div>
-            <h3>This is calender</h3>
+        <div className='flex mt-3  justify-center items-center '>
+            <div className='p-5 lg:p-20'>
+                <h3 className='text-center text-3xl text-primary font-bold'>Calendar</h3>
+                <div className='flex justify-center my-5'>
+                    <Calendar onChange={onChange} value={value} />
+                </div>
+            </div>
         </div>
     );
 };
 
-export default Calender;
+export default CalendarCompo;
